3db099888182fbe1a1d0ed64e0d58615c8060395
lukaszguz <lukasz.guz2@gmail.com>
2019-10-10 18:55:14 +0200

    2 Consumer

--- vertx-m1/src/main/kotlin/pl/guz/vertx/m1/eventbus/ConsumerVerticle.kt
+++ vertx-m1/src/main/kotlin/pl/guz/vertx/m1/eventbus/ConsumerVerticle.kt
@@ -1,11 +1,17 @@
 package pl.guz.vertx.m1.eventbus
 
+import io.vertx.core.json.Json
 import io.vertx.reactivex.core.AbstractVerticle
 import pl.guz.vertx.m1.logger
 
 class ConsumerVerticle : AbstractVerticle() {
 
     override fun start() {
+
+        vertx.eventBus().consumer<String>("jdd.consumer") {
+            logger.info("Message from producer: {}", Json.decodeValue(it.body(), Greetings::class.java))
+            it.reply("Siema!")
+        }
 
     }
 
